afcabeec9d61c435e2e4a438899d65a4769fbe06
radek-rozmus <radoslaw.rozmus@microsoft.wsei.edu.pl>
2021-03-07 18:39:45 +0100

    first works version

--- lab-project1/index.ts
+++ lab-project1/index.ts
@@ -1,72 +1,76 @@
 class App
 {
-num1: HTMLInputElement;
-num2: HTMLInputElement;
-num3: HTMLInputElement;
-num4: HTMLInputElement;
+num1!: HTMLInputElement;
+num2!: HTMLInputElement;
+num3!: HTMLInputElement;
+num4!: HTMLInputElement;
 
-sum: HTMLInputElement;
-avr: HTMLInputElement;
-min: HTMLInputElement;
-max: HTMLInputElement;
+sum!: HTMLInputElement;
+avr!: HTMLInputElement;
+min!: HTMLInputElement;
+max!: HTMLInputElement;
 
-num1Value: number;
-num2Value: number;
-num3Value: number;
-num4Value: number;
+num1Value!: number;
+num2Value!: number;
+num3Value!: number;
+num4Value!: number;
 
-sumValue: number;
-avrValue: number;
-minValue: number;
-maxValue: number;
+sumValue!: number;
+avrValue!: number;
+minValue!: number;
+maxValue!: number;
 
 constructor(){
     this.getElements();
     this.listenInputs();
-    debugger
 }
 
 getElements = () => {
-    this.num1 = document.querySelector('#num1');
-    this.num2 = document.querySelector('#num2');
-    this.num3 = document.querySelector('#num3');
-    this.num4 = document.querySelector('#num4');
 
-    this.sum = document.querySelector('#sum');
-    this.avr = document.querySelector('#avr');
-    this.min = document.querySelector('#min');
-    this.max = document.querySelector('#max');
+    this.num1 = document.querySelector('#num1') as HTMLInputElement;
+    this.num2 = document.querySelector('#num2') as HTMLInputElement;
+    this.num3 = document.querySelector('#num3') as HTMLInputElement;
+    this.num4 = document.querySelector('#num4') as HTMLInputElement;
 
-    this.num1Value = +this.num1.nodeValue;
-    this.num2Value = +this.num2.nodeValue;
-    this.num3Value = +this.num3.nodeValue;
-    this.num4Value = +this.num4.nodeValue;
+    this.sum = document.querySelector('#sum') as HTMLInputElement;
+    this.avr = document.querySelector('#avr') as HTMLInputElement;
+    this.min = document.querySelector('#min') as HTMLInputElement;
+    this.max = document.querySelector('#max') as HTMLInputElement;
+
+    this.num1Value = +this.num1.value;
+    this.num2Value = +this.num2.value;
+    this.num3Value = +this.num3.value;
+    this.num4Value = +this.num4.value;
 }
 
-operateAndAssign = () => {
+getElementsOperateAndAssign = () => {
+
+    this.getElements();
+
     this.sumValue = this.num1Value + this.num2Value + this.num3Value + this.num4Value;
     this.avrValue = this.sumValue/4;
     this.minValue = Math.min(this.num1Value, this.num2Value, this.num3Value, this.num4Value);
     this.maxValue = Math.max(this.num1Value, this.num2Value, this.num3Value, this.num4Value);
-    this.assignElements();
+
+    this.assignElements(this.sumValue, this.avrValue, this.minValue, this.maxValue);
 }
 
-assignElements()
+assignElements(_sum: number, _avr: number, _min: number, _max:number)
 {
-    this.sum.value = this.sumValue.toString();
-    this.avr.value = this.avrValue.toString();
-    this.min.value = this.minValue.toString();
-    this.max.value = this.maxValue.toString();
+    this.sum.value = _sum.toString();
+    this.avr.value = _avr.toString();
+    this.min.value = _min.toString();
+    this.max.value = _max.toString();
 }
 
 listenInputs(){
-    this.num1.addEventListener('input', () => this.operateAndAssign());
-    this.num2.addEventListener('input', () => this.operateAndAssign());
-    this.num3.addEventListener('input', () => this.operateAndAssign());
-    this.num4.addEventListener('input', () => this.operateAndAssign());
+    this.num1.addEventListener('input', () => this.getElementsOperateAndAssign());
+    this.num2.addEventListener('input', () => this.getElementsOperateAndAssign());
+    this.num3.addEventListener('input', () => this.getElementsOperateAndAssign());
+    this.num4.addEventListener('input', () => this.getElementsOperateAndAssign());
 }
 
 
 }
 
-const app: App = new App();
+const app = new App();
