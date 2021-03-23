cb519e96ea5c58bfa5753340ecb2cb5c0c6ad0fe
radek-rozmus <radoslaw.rozmus@microsoft.wsei.edu.pl>
2021-03-23 11:52:59 +0100

    objective buttons

--- lab-project2/index.js
+++ lab-project2/index.js
@@ -1,27 +1,61 @@
 var App = /** @class */ (function () {
     function App() {
+        var _this = this;
         this.root = document.getElementById('root');
-        debugger;
-        this.boomSound = new Sound("boom", "./sounds/boom.wav", this.root);
-        this.clapSound = new Sound("clap", "./sounds/clap.wav", this.root);
-        this.hihatSound = new Sound("hihat", "./sounds/hihat.wav", this.root);
-        this.kickSound = new Sound("kick", "./sounds/kick", this.root);
-        this.openhatSound = new Sound("openhat", "./sounds/openhat", this.root);
-        this.rideSound = new Sound("ride", "./sounds/ride", this.root);
-        this.snareSound = new Sound("snare", "./sounds/snare", this.root);
-        this.tinkSound = new Sound("tink", "./sounds/tink", this.root);
-        this.tomSound = new Sound("tom", "./sounds/tom", this.root);
+        this.drumkit = document.getElementById('drumkit');
+        this.sounds = [];
+        //create sound buttons
+        this.createSoundButtons = function () {
+            _this.boomSoundButton = new SoundButton("boom", "q", "./sounds/boom.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.boomSoundButton.key, sound: _this.boomSoundButton.sound });
+            _this.clapSoundButton = new SoundButton("clap", "w", "./sounds/clap.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.clapSoundButton.key, sound: _this.clapSoundButton.sound });
+            _this.hihatSoundButton = new SoundButton("hihat", "e", "./sounds/hihat.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.hihatSoundButton.key, sound: _this.hihatSoundButton.sound });
+            _this.kickSoundButton = new SoundButton("kick", "r", "./sounds/kick.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.kickSoundButton.key, sound: _this.kickSoundButton.sound });
+            _this.openhatSoundButton = new SoundButton("openhat", "t", "./sounds/openhat.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.openhatSoundButton.key, sound: _this.openhatSoundButton.sound });
+            _this.rideSoundButton = new SoundButton("ride", "a", "./sounds/ride.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.rideSoundButton.key, sound: _this.rideSoundButton.sound });
+            _this.snareSoundButton = new SoundButton("snare", "s", "./sounds/snare.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.snareSoundButton.key, sound: _this.snareSoundButton.sound });
+            _this.tinkSoundButton = new SoundButton("tink", "d", "./sounds/tink.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.tinkSoundButton.key, sound: _this.tinkSoundButton.sound });
+            _this.tomSoundButton = new SoundButton("tom", "f", "./sounds/tom.wav", _this.drumkit, _this.root, _this);
+            _this.sounds.push({ key: _this.tomSoundButton.key, sound: _this.tomSoundButton.sound });
+        };
+        this.createSoundButtons();
     }
     return App;
 }());
 var Sound = /** @class */ (function () {
-    function Sound(name, path, where) {
+    function Sound(name, key, path, where) {
+        var _this = this;
+        this.playSound = function () {
+            _this.sound.currentTime = 0;
+            _this.sound.play();
+        };
         this.name = name;
+        this.key = key;
         this.path = path;
         this.sound = document.createElement('audio');
         this.sound.dataset.sound = name;
+        this.sound.src = path;
         where.appendChild(this.sound);
     }
     return Sound;
+}());
+var SoundButton = /** @class */ (function () {
+    function SoundButton(name, key, path, whereButton, whereAudio, context) {
+        var _this = this;
+        this.key = key;
+        this.sound = new Sound(name, key, path, whereAudio);
+        this.soundButton = document.createElement('button');
+        this.soundButton.textContent = this.key;
+        this.soundButton.addEventListener("click", function () { return _this.sound.playSound(); });
+        whereButton.appendChild(this.soundButton);
+    }
+    return SoundButton;
 }());
 var app = new App();
--- lab-project2/index.ts
+++ lab-project2/index.ts
@@ -1,44 +1,102 @@
+type SoundObject = {
+    key: string,
+    sound: Sound
+}
+
 class App {
 
 root: HTMLDivElement = document.getElementById('root') as HTMLDivElement;
+drumkit: HTMLDivElement = document.getElementById('drumkit') as HTMLDivElement;
 
-boomSound: Sound;
-clapSound: Sound;
-hihatSound: Sound;
-kickSound: Sound;
-openhatSound: Sound;
-rideSound: Sound;
-snareSound: Sound;
-tinkSound: Sound;
-tomSound: Sound;
+boomSoundButton!: SoundButton;
+clapSoundButton!: SoundButton;
+hihatSoundButton!: SoundButton;
+kickSoundButton!: SoundButton;
+openhatSoundButton!: SoundButton;
+rideSoundButton!: SoundButton;
+snareSoundButton!: SoundButton;
+tinkSoundButton!: SoundButton;
+tomSoundButton!: SoundButton;
+
+sounds: SoundObject[] = [];
 
 constructor(){
-this.boomSound = new Sound("boom", "./sounds/boom.wav", this.root);
-this.clapSound = new Sound("clap", "./sounds/clap.wav", this.root);
-this.hihatSound = new Sound("hihat", "./sounds/hihat.wav", this.root);
-this.kickSound = new Sound("kick", "./sounds/kick", this.root);
-this.openhatSound = new Sound("openhat", "./sounds/openhat", this.root);
-this.rideSound = new Sound("ride", "./sounds/ride", this.root);
-this.snareSound = new Sound("snare", "./sounds/snare", this.root);
-this.tinkSound = new Sound("tink", "./sounds/tink", this.root);
-this.tomSound = new Sound("tom", "./sounds/tom", this.root);
+
+this.createSoundButtons();
+
 }
 
+//create sound buttons
+createSoundButtons = () => {
 
+this.boomSoundButton = new SoundButton("boom", "q", "./sounds/boom.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.boomSoundButton.key, sound: this.boomSoundButton.sound});
+
+this.clapSoundButton = new SoundButton("clap", "w", "./sounds/clap.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.clapSoundButton.key, sound: this.clapSoundButton.sound});
+
+this.hihatSoundButton = new SoundButton("hihat", "e", "./sounds/hihat.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.hihatSoundButton.key, sound: this.hihatSoundButton.sound});
+
+this.kickSoundButton = new SoundButton("kick", "r", "./sounds/kick.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.kickSoundButton.key, sound: this.kickSoundButton.sound});
+
+this.openhatSoundButton = new SoundButton("openhat", "t", "./sounds/openhat.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.openhatSoundButton.key, sound: this.openhatSoundButton.sound});
+
+this.rideSoundButton = new SoundButton("ride", "a", "./sounds/ride.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.rideSoundButton.key, sound: this.rideSoundButton.sound});
+
+this.snareSoundButton = new SoundButton("snare", "s", "./sounds/snare.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.snareSoundButton.key, sound: this.snareSoundButton.sound});
+
+this.tinkSoundButton = new SoundButton("tink", "d", "./sounds/tink.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.tinkSoundButton.key, sound: this.tinkSoundButton.sound});
+
+this.tomSoundButton = new SoundButton("tom", "f", "./sounds/tom.wav", this.drumkit, this.root, this);
+this.sounds.push({key: this.tomSoundButton.key, sound: this.tomSoundButton.sound});
+
+}
 
 }
 
 class Sound {
     name: string;
+    key: string;
     path: string;
     sound: HTMLAudioElement;
 
-    constructor(name: string, path: string, where: HTMLDivElement){
+    constructor(name: string, key: string, path: string, where: HTMLDivElement){
         this.name = name;
+        this.key = key;
         this.path = path;
         this.sound = document.createElement('audio');
         this.sound.dataset.sound = name;
+        this.sound.src = path;
         where.appendChild(this.sound);
+    }
+
+    playSound = () => {
+        this.sound.currentTime = 0;
+        this.sound.play()
+    }
+
+}
+
+class SoundButton {
+    key: string;
+    soundButton: HTMLButtonElement;
+    sound: Sound;
+
+    constructor(name: string, key: string, path:string, whereButton: HTMLDivElement, whereAudio: HTMLDivElement, context: App){
+        this.key = key;
+
+        this.sound = new Sound(name, key, path, whereAudio);
+
+        this.soundButton = document.createElement('button');
+        this.soundButton.textContent = this.key;
+        this.soundButton.addEventListener("click", () => this.sound.playSound());
+        whereButton.appendChild(this.soundButton);
     }
 
 }
