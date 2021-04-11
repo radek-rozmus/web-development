d3b73f6f3762c3c5eacf76cf93cf10854537dbcf
radek-rozmus <radoslaw.rozmus@microsoft.wsei.edu.pl>
2021-04-11 14:23:01 +0200

    bad version

--- lab-project2/index.js
+++ lab-project2/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var App = /** @class */ (function () {
     function App() {
         var _this = this;
---
+++ lab-project3/index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var App = /** @class */ (function () {
+    function App() {
+        this.root = document.getElementById('root');
+    }
+    return App;
+}());
+var app = new App();
